6d8b2bbf96166961098ff7efdb0b3ad0c7d5a95c
Menocas17 <rodolfomenocal7@gmail.com>
2025-03-11 21:45:43 -0600

    adding the message in the home page

--- Project-1/routes/contact.mjs
+++ Project-1/routes/contact.mjs
@@ -2,6 +2,11 @@ import express from 'express';
 import {getAllContacts, getContactByName} from '../controllers/contactController.mjs'
 const router = express.Router();
 
+// home router
+
+router.get('/', (req, res) => {
+    res.send('Hellou CSE 341')
+})
 // route for getting all the contacts list
 router.get('/get-all', getAllContacts);
 
