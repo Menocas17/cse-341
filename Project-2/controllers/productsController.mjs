f2907a5312d318c7ae9f28d21ad01f5b8cd40922
Menocas17 <rodolfomenocal7@gmail.com>
2025-04-01 15:23:33 -0600

    controllers for the products created

---
+++ Project-2/controllers/productsController.mjs
@@ -0,0 +1,113 @@
+import { productModel } from '../models/productModel.mjs';
+
+// getting all the products
+
+export async function getAllProducts(req, res, next) {
+  try {
+    const allProducts = await productModel.find({});
+
+    res.status(200).json(allProducts);
+  } catch (err) {
+    next(err);
+  }
+}
+
+//get product by id
+
+export async function productById(req, res, next) {
+  const { product_id } = req.params;
+
+  try {
+    const product = await productModel.findById(product_id);
+
+    if (!product) {
+      res.status(404).json({ message: 'Product not found' });
+    }
+
+    res.status(200).json(product);
+  } catch (err) {
+    next(err);
+  }
+}
+
+//create a new product, once authentication is implemented this is going to be available just to employess and admins
+
+export async function createProduct(req, res, next) {
+  const {
+    product_brand,
+    product_color,
+    product_description,
+    product_name,
+    product_price,
+  } = req.body;
+
+  try {
+    const newProduct = new productModel({
+      product_brand,
+      product_color,
+      product_description,
+      product_name,
+      product_price,
+    });
+
+    await newProduct.save();
+
+    res.status(201).json({
+      message: `New product ${newProduct.product_name} successfully created`,
+      product_id: newProduct._id,
+    });
+  } catch (err) {
+    next(err);
+  }
+}
+
+//updating product by id
+
+export async function updateProduct(req, res, next) {
+  const { product_id } = req.params;
+  const updatedInfo = {
+    product_brand: req.body.product_brand,
+    product_color: req.body.product_color,
+    product_description: req.body.product_description,
+    product_name: req.body.product_name,
+    product_price: req.body.product_price,
+  };
+
+  try {
+    const updatedProduct = await productModel.findByIdAndUpdate(
+      product_id,
+      updatedInfo,
+      { new: true }
+    );
+    if (!updatedProduct) {
+      res.status(404).json({ message: 'Product not found' });
+    }
+
+    res.status(200).json({
+      message: 'Product successfully updated',
+      updatedUser: updatedProduct,
+    });
+  } catch (err) {
+    next(err);
+  }
+}
+
+//deleting products
+
+export async function deleteProduct(req, res, next) {
+  const { product_id } = req.params;
+
+  try {
+    const deletedProduct = await productModel.findByIdAndDelete(product_id);
+
+    if (!deletedProduct) {
+      res.status(404).json({ message: 'Product not found' });
+    }
+
+    res
+      .status(200)
+      .json({ message: `Product with id: ${product_id} successfully deleted` });
+  } catch (err) {
+    nect(err);
+  }
+}
--- Project-2/routers/products.mjs
+++ Project-2/routers/products.mjs
@@ -1,30 +1,27 @@
 import { Router } from 'express';
+import {
+  getAllProducts,
+  productById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+} from '../controllers/productsController.mjs';
 const router = Router();
 
 //getting all products route
-router.get('/', (req, res) => {
-  res.send('Testing routes');
-});
+router.get('/', getAllProducts);
 
 //getting product by id
-router.get('/:product_id', (req, res) => {
-  res.send('Testing routes');
-});
+router.get('/:product_id', productById);
 
 //Creating a new product
-router.post('/create', (req, res) => {
-  res.send('Testing routes');
-});
+router.post('/create', createProduct);
 
 //updating product by id
-router.put('/update/:product_id', (req, res) => {
-  res.send('Testing routes');
-});
+router.put('/update/:product_id', updateProduct);
 
 //deleting product by id
 
-router.delete('/delete/:product_id', (req, res) => {
-  res.send('Testing routes');
-});
+router.delete('/delete/:product_id', deleteProduct);
 
 export default router;
